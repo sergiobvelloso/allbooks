c36216d1e6f631eab6ec603935cbcaca66abb107
Sergio Velloso <sergiolbvelloso@gmail.com>
2025-08-09 10:49:22 -0300

    Altera o server

--- src/server.js
+++ src/server.js
@@ -18,10 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas-vindas à API ALLBOOKS")
-  console.log("API disponível através da URL http://localhost:8000")
-=======
-  console.log("Acesse essa API em http://localhost:8000")
->>>>>>> 40a8202c17e93fd5f7e4dc7eee883a4b23bc2e03
+  console.log("Disponível em http://localhost:8000")
 })
